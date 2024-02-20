6698bba3cca8add2bcbc0fe49847035a95fb3dcc
PhoenixxZ2023 <101994539+PhoenixxZ2023@users.noreply.github.com>
2024-02-20 19:22:19 -0300

    Add files via upload

--- speedtestvps.sh
+++ speedtestvps.sh
@@ -3,7 +3,28 @@
 # Definir cores
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
+WHITE='\033[1;37m'
 NC='\033[0m' # No Color
+
+# Função para desenhar um retângulo com o texto centralizado
+function draw_rectangle {
+    local text="$1"
+    local length=${#text}
+    local total_length=50
+    local side_length=$(( (total_length - length) / 2 ))
+    printf "\n${WHITE}┌─"
+    for ((i = 0; i < side_length; i++)); do
+        printf "─"
+    done
+    printf "${GREEN}$text${WHITE}"
+    for ((i = 0; i < side_length; i++)); do
+        printf "─"
+    done
+    if (( (total_length - length) % 2 != 0 )); then
+        printf "─"
+    fi
+    printf "─┐${NC}\n"
+}
 
 # Verificar se o speedtest-cli já está instalado
 if [ -f "speedtest-cli" ]; then
@@ -12,7 +33,7 @@ else
     echo "================================================="
     echo "            Baixando script de teste de velocidade"
     echo "================================================="
-    wget -O speedtest-cli https://raw.githubusercontent.com/PhoenixxZ2023/speedtest-vps/master/speedtestvps.sh
+    wget -O speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py
     chmod +x speedtest-cli
     echo "Script de teste de velocidade instalado com sucesso!"
 fi
@@ -26,19 +47,16 @@ function test_speed {
     echo -e "${GREEN}Utilizando o servidor $server_id ($server_ip)${NC}"
     echo "$speedtest_result"
     echo -e "${GREEN}Link do resultado: https://www.speedtest.net/result/$(./speedtest-cli --share | grep -oP 'https://www.speedtest.net/result/\S+')${NC}"
-    read -p "${YELLOW}Pressione Enter para retornar ao menu principal...${NC}"
+    read -p "${YELLOW}Pressione Enter para retornar ao menu principal...${NC}" enter
 }
 
 # Exibir menu
 function show_menu {
     clear
-    echo -e "${GREEN}=================================================${NC}"
-    echo -e "${GREEN}                Speedtest Menu                ${NC}"
-    echo -e "${GREEN}=================================================${NC}"
+    draw_rectangle "Speedtest Menu"
     echo -e "${YELLOW}Selecione uma opção:${NC}"
     echo -e "${YELLOW}1. Testar velocidade da internet${NC}"
     echo -e "${YELLOW}2. Sair${NC}"
-    echo -e "${GREEN}=================================================${NC}"
     read -p "${YELLOW}Opção: ${NC}" option
 
     # Executar a ação baseada na opção escolhida
@@ -52,6 +70,7 @@ function show_menu {
             ;;
         *)
             echo -e "${YELLOW}Opção inválida. Por favor, escolha uma opção válida.${NC}"
+            read -p "${YELLOW}Pressione Enter para retornar ao menu principal...${NC}" enter
             ;;
     esac
 }
